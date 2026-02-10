0893f11b3ef0b5d5740a0d7724afb9c7ae504146
Hugo <zzzhugo@icloud.com>
2026-02-09 23:24:13 -0500

    changé scene en QGraphicsScene et ajout de classe: point cliquable pour mettre les éléments du circuit

--- Circuit.py
+++ Circuit.py
@@ -1,7 +1,7 @@
-from PySide6.QtCore import QSize, QPoint
-from PySide6.QtGui import QIcon, QPixmap, QColorConstants, QPainter, QPen, Qt
+from PySide6.QtCore import QSize, QPoint, QPointF
+from PySide6.QtGui import QIcon, QPixmap, QColorConstants, QPainter, QPen, Qt, QPainterPath, QBrush
 from PySide6.QtWidgets import QMainWindow, QToolBar, QWidget, QApplication, QPushButton, QVBoxLayout, QLabel, \
-    QGraphicsPixmapItem, QGraphicsScene
+    QGraphicsPixmapItem, QGraphicsScene, QGraphicsView, QGraphicsPathItem, QGraphicsEllipseItem
 
 from Dispositifs import toolbar_dispositifs
 
@@ -18,62 +18,107 @@ class Circuit(QMainWindow):
         toolbar = QToolBar()
         self.addToolBar(toolbar)
 
-        for dispositif in toolbar_dispositifs:
+        for dispositif in toolbar_dispositifs.values():
             nom = dispositif.nom
             bouton = QPushButton()
             bouton.setIcon(QIcon(dispositif.image_toolbar))
             bouton.setIconSize(QSize(45, 45))
+
+            bouton.clicked.connect(lambda _, x=dispositif: self.toolbar_clicked(x))
+
             toolbar.addWidget(bouton)
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout = QVBoxLayout()
-        self.central_widget.setLayout(self.layout)
-
-        self.scene = QLabel()
-        self.layout.addWidget(self.scene)
+        # Fenetre de jeu
+        self.vue = QGraphicsView()
+        self.scene = QGraphicsScene()
+        self.vue.setScene(self.scene)
+        self.setCentralWidget(self.vue)
 
         self.dessiner_fond()
         self.dessiner_circuit_base()
 
+    def toolbar_clicked(self, dispositif):
+        self.selection = dispositif
+
     def dessiner_fond(self):
-        self.canevas = QPixmap(self.scene_size)
-        self.canevas.fill(QColorConstants.White)
-        self.scene.setPixmap(self.canevas)
+        self.scene.setBackgroundBrush(QColorConstants.White)
+        self.scene.setSceneRect(0, 0, self.scene_size.width(), self.scene_size.height())
 
     def dessiner_circuit_base(self):
-        painter = QPainter(self.canevas)
-        crayon = QPen()
-        crayon.setWidth(5)
-        crayon.setColor(QColorConstants.Black)
-        painter.setPen(crayon)
+        center_x = self.scene.width() / 2
+        center_y = self.scene.height() / 2
 
-        largeur = 300
+        largeur = 400
         hauteur = 200
 
-        hautgauche = QPoint(250 - round(largeur / 2), 250 - round(hauteur / 2))
-        hautdroite = QPoint(250 + round(largeur / 2), 250 - round(hauteur / 2))
-        basgauche = QPoint(250 - round(largeur / 2), 250 + round(hauteur / 2))
-        basdroite = QPoint(250 + round(largeur / 2), 250 + round(hauteur / 2))
+        circuit_fil = QGraphicsPathItem()
+        self.scene.addItem(circuit_fil)
 
-        painter.drawLine(hautgauche, hautdroite)
-        painter.drawLine(hautdroite, basdroite)
-        painter.drawLine(basdroite, basgauche)
-        painter.drawLine(basgauche, hautgauche)
+        pen = QPen()
+        pen.setColor(QColorConstants.Black)
+        pen.setWidth(2)
+        circuit_fil.setPen(pen)
 
-        # faire en sorte quon peut aller le chercher comme dans une bibliotheque (toolbar_dispositif["Batterie"])
-        batterie_pixmap = QPixmap(toolbar_dispositifs[0].image_circuit)
-        scaled_batterie_pixmap = batterie_pixmap.scaled(QSize(64, 64), aspectMode=Qt.AspectRatioMode.KeepAspectRatio)
+        path = QPainterPath()
 
-        center_pix = QPoint(round(self.scene_size.width() / 2 - scaled_batterie_pixmap.width() / 2),
-                            round(250 - round(hauteur / 2) - scaled_batterie_pixmap.height() / 2))
+        def trouver_point(x, y):
+            return QPointF(center_x + x * largeur / 2, center_y + y * hauteur / 2)
 
-        painter.drawPixmap(center_pix, scaled_batterie_pixmap)
+        #Prépare le path en haut à gauche
+        path.moveTo(trouver_point(-1, -1))
 
-        painter.end()
+        path.lineTo(trouver_point(1, -1))
+        path.lineTo(trouver_point(1, 1))
+        path.lineTo(trouver_point(-1, 1))
+        path.lineTo(trouver_point(-1, -1))
 
-        self.scene.setPixmap(self.canevas)
+        path.closeSubpath()
+
+        circuit_fil.setPath(path)
+
+        batterie = toolbar_dispositifs["Batterie"]
+        self.ajouter_img_circuit(batterie.image_circuit, center_x, center_y - hauteur / 2, batterie.scale)
+
+        pen = QPen()
+        pen.setColor(QColorConstants.Black)
+        pen.setWidth(4)
+        brush = QBrush()
+        brush.setColor(QColorConstants.White)
+        brush.setStyle(Qt.SolidPattern)
+
+        diametre = 25
+        
+        circle = CercleCliquable(center_x, center_y + hauteur/2, diametre, self)
+        circle.setPen(pen)
+        circle.setBrush(brush)
+
+        self.scene.addItem(circle)
+
+    def ajouter_img_circuit(self, image, pos_x, pos_y, scale):
+
+        pixmap = QPixmap(image)
+        pixmap_scaled = pixmap.scaled(scale, scale, Qt.AspectRatioMode.KeepAspectRatio)
+
+        pixmap_item = QGraphicsPixmapItem(pixmap_scaled)
+        pixmap_item.setPos(QPointF(pos_x - pixmap_scaled.width()/2, pos_y - pixmap_scaled.height()/2))
+
+        self.scene.addItem(pixmap_item)
+
+    def bouton_cercle_click(self, cercle):
+        if self.selection is not None:
+            self.scene.removeItem(cercle)
+            self.ajouter_img_circuit(self.selection.image_circuit, cercle.x, cercle.y, self.selection.scale)
+
+
+class CercleCliquable(QGraphicsEllipseItem):
+    def __init__(self, x, y, diametre, main_window):
+        super().__init__(x - diametre/2, y - diametre/2, diametre, diametre)
+        self.main_window = main_window
+        self.x = x
+        self.y = y
+
+    def mousePressEvent(self, event):
+        self.main_window.bouton_cercle_click(self)
 
 
 if __name__ == "__main__":
--- Dispositifs.py
+++ Dispositifs.py
@@ -1,41 +1,64 @@
-from PySide6.QtWidgets import QMainWindow
-
-
-
 class Batterie:
     def __init__(self):
         self.nom = "Batterie"
-        self.image_toolbar = "images/batterie.jpg"
-        self.image_circuit = "images/pile_circuit.png"
+        self.image_toolbar = "images/toolbar/batterie.jpg"
+        self.image_circuit = "images/circuit/batterie.png"
+        self.scale = 40
+
 
 class LED:
     def __init__(self):
         self.nom = "LED"
-        self.image_toolbar = "images/LED.webp"
+        self.image_toolbar = "images/toolbar/LED.webp"
+        self.image_circuit = "images/circuit/LED.png"
+        self.scale = 78
+
 
 class Resistor:
     def __init__(self):
         self.nom = "Resistor"
-        self.image_toolbar = "images/Resistor.jpg"
+        self.image_toolbar = "images/toolbar/resistor.jpg"
+        self.image_circuit = "images/circuit/resistor.png"
+        self.scale = 55
+
 
 class Diode:
     def __init__(self):
         self.nom = "Diode"
-        self.image_toolbar = "images/diode.jpg"
+        self.image_toolbar = "images/toolbar/diode.jpg"
+        self.image_circuit = "images/circuit/diode.png"
+        self.scale = 30
+
 
 class Interrupteur:
     def __init__(self):
         self.nom = "Interrupteur"
-        self.image_toolbar = "images/interrupteur.jpg"
+        self.image_toolbar = "images/toolbar/interrupteur.jpg"
+        self.image_circuit = "images/circuit/interrupteur_ouvert.png"
+        self.scale = 45
+
 
 class Voltmetre:
     def __init__(self):
         self.nom = "Voltmetres"
-        self.image_toolbar = "images/voltmetre.jpg"
+        self.image_toolbar = "images/toolbar/voltmetre.jpg"
+        self.image_circuit = "images/circuit/voltmetre.png"
+        self.scale = 40
+
 
 class Amperemetre:
     def __init__(self):
         self.nom = "Amperemetre"
-        self.image_toolbar = "images/amperemetre.jpg"
+        self.image_toolbar = "images/toolbar/amperemetre.jpg"
+        self.image_circuit = "images/circuit/amperemetre.png"
+        self.scale = 40
 
-toolbar_dispositifs = [Batterie(), LED(), Resistor(), Diode(), Interrupteur(), Voltmetre(), Amperemetre()]
+
+toolbar_dispositifs = {
+    "Batterie": Batterie(),
+    "LED": LED(),
+    "Resistor": Resistor(),
+    "Diode": Diode(),
+    "Interrupteur": Interrupteur(),
+    "Voltmetre": Voltmetre(),
+    "Amperemetre": Amperemetre()}
