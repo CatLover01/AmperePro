5d3e32a36af7ec96f27e5f1e8374da8c1cd8f0d9
ElliotLalancette <elliotrl7@icloud.com>
2026-02-26 16:28:22 -0500

    création bd

--- bd.py
+++ bd.py
@@ -4,3 +4,6 @@ bd = {
     "Circuits": [
         {"Nom": "JM L\n'électricité", "Données": ["A", "B", "C"], "dernière sauvegarde" : "19 avril 1962"}
         ],}
+
+with open("bd.json", "w", encoding = "UTF-8") as f:
+    json.dump(bd, f, indent = 4)
