0e50e870f6abe765f4eaf11a74e32d7506981090
Hugo <zzzhugo@icloud.com>
2026-04-02 12:19:31 -0400

    ajouter des fils et leurs points dans la matrice

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-from PySide6.QtCore import QSize, QPointF, QRect, QFile, QTextStream
+from PySide6.QtCore import QSize, QPointF, QRect, QFile, QTextStream, QLineF
 from PySide6.QtGui import QColorConstants, QPen, Qt, QBrush, QAction, QIcon
-from PySide6.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView, QPushButton, QDialog, QHBoxLayout
+from PySide6.QtWidgets import (QMainWindow, QApplication, QGraphicsScene, QGraphicsView, QPushButton, QDialog,
+                               QHBoxLayout)
 import math
 import numpy as np
-import Composantes
 from a_propos import AProposWindow
 from docs import DocumentationWindow
 
@@ -36,7 +36,7 @@ class Window(QMainWindow):
         self.largeur = self.scene_size.width()
         self.hauteur = self.scene_size.height()
         # La distance entre chaque ligne dans le grid
-        self.taille_grid = 14
+        self.taille_grid = 20
 
         self.mat_i0 = 0
         self.mat_j0 = 0
@@ -44,7 +44,6 @@ class Window(QMainWindow):
 
         largeur_fil_base = 200
         hauteur_fil_base = 100
-        fil_base, self.mat_points = self.dessiner_circuit_base(largeur_fil_base, hauteur_fil_base)
 
         self.debut_matrice_i = None
         self.debut_matrice_j = None
@@ -52,9 +51,14 @@ class Window(QMainWindow):
         self.dessine = False
         self.dernier_point = None
 
-        self.pivot_index = None
         self.lignes = []
-        self.fils = []
+        self.points_apres_pivot = []
+        self.points_avant_pivot = []
+        self.fil_touche = None
+        self.fil_complet = False
+
+        fil_base, self.mat_points = self.dessiner_circuit_base(largeur_fil_base, hauteur_fil_base)
+        self.fils = [fil_base]
 
         #Menubar
         barre_menu = self.menuBar()
@@ -238,10 +242,11 @@ class Window(QMainWindow):
         gauche, haut = self.pos_selon_grid(QPointF(gauche, haut))
         droite, bas = self.pos_selon_grid(QPointF(droite, bas))
 
-        lignes_haut = []
-        lignes_droite = []
-        lignes_bas = []
-        lignes_gauche = []
+        ligne_haut = self.ajouter_ligne(gauche, haut, droite, haut)
+        ligne_droite = self.ajouter_ligne(droite, haut, droite, bas)
+        ligne_bas = self.ajouter_ligne(droite, bas, gauche, bas)
+        ligne_gauche = self.ajouter_ligne(gauche, bas, gauche, haut)
+        fil_base = Fil(self, [ligne_haut, ligne_droite, ligne_bas, ligne_gauche])
 
         nb_points_x = round((droite - gauche) / self.taille_grid) + 1
         nb_points_y = round((bas - haut) / self.taille_grid) + 1
@@ -251,114 +256,273 @@ class Window(QMainWindow):
         matrice_points = np.zeros((nb_points_y, nb_points_x))
 
         # lignes verticales
-        for i in range(nb_points_y - 1):
-            yi = haut + i * self.taille_grid
-            y = yi + self.taille_grid
-            lignes_droite.append(self.ajouter_ligne(droite, yi, droite, y))
+        for k in range(nb_points_y):
+            y = haut + k * self.taille_grid
 
             mat_i, mat_j = self.pos_to_mat(droite, y)
             matrice_points[mat_i, mat_j] = 1
-
-            yi = bas - i * self.taille_grid
-            y = yi - self.taille_grid
-            lignes_gauche.append(self.ajouter_ligne(gauche, yi, gauche, y))
 
             mat_i, mat_j = self.pos_to_mat(gauche, y)
             matrice_points[mat_i, mat_j] = 1
 
         # lignes horizontales
-        for i in range(nb_points_x - 1):
-            xi = gauche + i * self.taille_grid
-            x = xi + self.taille_grid
-            lignes_haut.append(self.ajouter_ligne(xi, haut, x, haut))
+        for k in range(nb_points_x - 2):
+            x = gauche + (k + 1) * self.taille_grid
 
             mat_i, mat_j = self.pos_to_mat(x, haut)
             matrice_points[mat_i, mat_j] = 1
 
-            xi = droite - i * self.taille_grid
-            x = xi - self.taille_grid
-            lignes_bas.append(self.ajouter_ligne(xi, bas, x, bas))
-
             mat_i, mat_j = self.pos_to_mat(x, bas)
             matrice_points[mat_i, mat_j] = 1
 
-        lignes_fil = lignes_haut + lignes_droite + lignes_bas + lignes_gauche
-        fil_base = Fil(self, lignes_fil)
-        self.fils.append(fil_base)
         return fil_base, matrice_points
 
     def verifier_collision_fil(self, pos):
         x, y = self.pos_selon_grid(pos)
-        mat_i, mat_j = self.pos_to_mat(x, y)
+        pos_i, pos_j = self.pos_to_mat(x, y)
 
         max_i = self.mat_points.shape[0] - 1
         max_j = self.mat_points.shape[1] - 1
 
-        if mat_i <= max_i and mat_i >= 0 and mat_j <= max_j and mat_j >= 0:
-            return round(self.mat_points[mat_i, mat_j])
+        if QPointF(x, y) == self.dernier_point:
+            return None
+        elif pos_i <= max_i and pos_i >= 0 and pos_j <= max_j and pos_j >= 0:
+            return round(self.mat_points[pos_i, pos_j])
         else:
             return 0
 
-    def demarrer_ligne(self, pos):
-        fil = self.verifier_collision_fil(pos)
+    def clic_gauche_fil(self, pos):
+        def mettre_index_mat(debut_ligne, fin_ligne):
+
+            diff_x = fin_ligne.x() - debut_ligne.x()
+            diff_y = fin_ligne.y() - debut_ligne.y()
+            if abs(diff_x) > abs(diff_y):
+                sens_x = 1
+                if diff_x < 0:
+                    sens_x = -1
+                sens_y = 0
+            else:
+                sens_y = 1
+                if diff_y < 0:
+                    sens_y = -1
+                sens_x = 0
+
+            nb_points = round((diff_x * sens_x + diff_y * sens_y) / self.taille_grid)
+            for k in range(nb_points):
+                debut_ligne = self.lignes[-1].line().p1()
+                fin_ligne = self.lignes[-1].line().p2()
+
+                pos_x = debut_ligne.x() + (k + 1) * self.taille_grid * sens_x
+                pos_y = debut_ligne.y() + (k + 1) * self.taille_grid * sens_y
+                i, j = self.pos_to_mat(pos_x, pos_y)
+                i, j = self.agrandir_matrice(i, j)
+                self.mat_points[i, j] = len(self.fils) + 1
+
+        touche = self.verifier_collision_fil(pos)
+
         if not self.dessine:
-            if fil != 0:
+            if touche != 0:
                 self.dessine = True
                 x, y = self.pos_selon_grid(pos)
-                mat_i, mat_j = self.pos_to_mat(x, y)
+                ligne = self.ajouter_ligne(x, y, x, y)
+                self.lignes.append(ligne)
+                self.dernier_point = QPointF(x, y)
 
-                self.dernier_point = [mat_i, mat_j]
+                i, j = self.pos_to_mat(x, y)
+                self.mat_points[i, j] = len(self.fils) + 1
+                self.touche = touche
+                self.points_avant_pivot.append(QPointF(x, y))
+
+        elif self.fil_complet:
+            debut_ligne = self.lignes[-1].line().p1()
+            fin_ligne = self.lignes[-1].line().p2()
+            mettre_index_mat(debut_ligne, fin_ligne)
+
+            # Ajouter noeud a premier et dernier point
+
+            nouveau_fil = Fil(self, self.lignes)
+            self.fils.append(nouveau_fil)
+            self.lignes = []
+            self.points_avant_pivot = []
+            self.points_avant_pivot = []
+            self.dessine = False
+
+            # TODO Ajouter noeuds
         else:
-            self.pivot_index = len(self.lignes) - 1
+            debut_ligne = self.lignes[-1].line().p1()
+            fin_ligne = self.lignes[-1].line().p2()
+            mettre_index_mat(debut_ligne, fin_ligne)
+            ligne = self.ajouter_ligne(fin_ligne.x(), fin_ligne.y(), fin_ligne.x(), fin_ligne.y())
 
-    def agrandir_matrice(self, sens):
+            self.points_avant_pivot += self.points_apres_pivot
+            self.points_apres_pivot = []
+            self.lignes.append(ligne)
+            self.dernier_point = QPointF(fin_ligne.x(), fin_ligne.y())
+            self.continuer_dessin(pos)
+
+    def clic_droit_fil(self):
+        if self.dessine:
+            for ligne in self.lignes:
+
+                pos_ligne_x, pos_ligne_y = ligne.line().p2().x(), ligne.line().p2().y()
+                mat_i, mat_j = self.pos_to_mat(pos_ligne_x, pos_ligne_y)
+                self.mat_points[mat_i, mat_j] = 0
+                self.scene.removeItem(ligne)
+
+            points = self.points_avant_pivot + self.points_apres_pivot
+            for point in points:
+                i, j = self.pos_to_mat(point.x(), point.y())
+                if point == points[0]:
+                    self.mat_points[i, j] = self.touche
+                else:
+                    self.mat_points[i, j] = 0
+
+            self.rapetisser_matrice()
+            self.dessine = False
+            self.lignes = []
+
+    def agrandir_matrice(self, i, j):
+        def agrandir(i_ajout, j_ajout, position, axe):
+            matrice_ajout = np.zeros((i_ajout, j_ajout))
+            self.mat_points = np.insert(self.mat_points, position, matrice_ajout.flatten(), axis=axe)
+
         mat_size_i, mat_size_j = self.mat_points.shape
-        if sens == "gauche":
-            axe = 1
-            i = mat_size_i
-            j = 1
-            position = 0
+        if j < 0:
+            agrandir(mat_size_i, 1, 0, 1)
+            self.mat_j0 -= self.taille_grid
+            return i, j + 1
 
-            self.debut_matrice_i -= self.taille_grid
+        elif j > mat_size_j - 1:
+            agrandir(mat_size_i, 1, mat_size_j, 1)
 
-        elif sens == "droite":
-            axe = 1
-            i = mat_size_i
-            j = 1
-            position = mat_size_j
+        elif i < 0:
+            agrandir(1, mat_size_j, 0, 0)
+            self.mat_i0 -= self.taille_grid
+            return i + 1, j
 
-        elif sens == "haut":
-            axe = 0
-            i = 1
-            j = mat_size_j
-            position = 0
+        elif i > mat_size_i - 1:
+            agrandir(1, mat_size_j, mat_size_i, 0)
 
-        else:
-            axe = 0
-            i = 1
-            j = mat_size_j
-            position = mat_size_i
+        return i, j
 
-            self.debut_matrice_j -= self.taille_grid
+    def rapetisser_matrice(self):
+        len_i = self.mat_points.shape[0]
+        len_j = self.mat_points.shape[1]
 
-        matrice_ajout = np.zeros((i, j))
-        self.mat_points = np.insert(self.mat_points, position, matrice_ajout.flatten(), axis=axe)
+        for i in reversed(range(len_i)):
+            ligne_vide = True
+            for j in range(len_j):
+                if self.mat_points[i, j] != 0:
+                    ligne_vide = False
+                    break
+            if ligne_vide:
+                self.mat_points = np.delete(self.mat_points, i, 0)
+                len_i -= 1
+            else:
+                break
+
+        for i in range(len_i):
+            ligne_vide = True
+            for j in range(len_j):
+                if self.mat_points[0, j] != 0:
+                    ligne_vide = False
+                    break
+            if ligne_vide:
+                self.mat_points = np.delete(self.mat_points, 0, 0)
+                self.mat_i0 += self.taille_grid
+                len_i -= 1
+            else:
+                break
+
+        for j in reversed(range(len_j)):
+            colonne_vide = True
+            for i in range(len_i):
+                if self.mat_points[i, j] != 0:
+                    colonne_vide = False
+                    break
+            if colonne_vide:
+                self.mat_points = np.delete(self.mat_points, j, 1)
+                len_j -= 1
+            else:
+                break
+
+        for j in range(len_j):
+            colonne_vide = True
+            for i in range(len_i):
+                if self.mat_points[i, 0] != 0:
+                    colonne_vide = False
+                    break
+            if colonne_vide:
+                self.mat_points = np.delete(self.mat_points, 0, 1)
+                self.mat_j0 += self.taille_grid
+            else:
+                break
 
     def continuer_dessin(self, pos):
-
         collision = self.verifier_collision_fil(pos)
-        if collision == 0:
-            # TODO: Vérifier dans quel sens l'utilisateur souhaite aller et ajouter les fils là
-            # Agrandir la matrice au besoin
-            pass
-        elif collision == len(self.fils) - 1:
-            # TODO: Gérer les fils quand l'utilisateur revient sur le même fil
-            # Rétrécir la matrice au besoin
+
+        if collision is None:
             pass
 
         else:
-            # TODO: Ajouter un noeud puisque le fil touche à un autre fil
-            pass
+            curs_x, curs_y = self.pos_selon_grid(pos)
+
+            ligne = self.lignes[-1]
+            pivot = ligne.line().p1()
+
+            diff_x = curs_x - pivot.x()
+            diff_y = curs_y - pivot.y()
+
+            if abs(diff_x) > abs(diff_y):
+                sens_x = 1
+                if diff_x < 0:
+                    sens_x = -1
+                sens_y = 0
+                x = curs_x
+                y = pivot.y()
+
+            else:
+                sens_y = 1
+                if diff_y < 0:
+                    sens_y = -1
+                sens_x = 0
+                x = pivot.x()
+                y = curs_y
+
+            dernier_point = ligne.line().p2()
+            if QPointF(x, y) != dernier_point:
+                self.fil_complet = False
+
+                ligne_p2_x = pivot.x() + self.taille_grid * sens_x
+                ligne_p2_y = pivot.y() + self.taille_grid * sens_y
+
+                ligne_i, ligne_j = self.pos_to_mat(ligne_p2_x, ligne_p2_y)
+
+                while True:
+                    ligne_i, ligne_j = self.agrandir_matrice(ligne_i, ligne_j)
+                    mat_num = self.mat_points[ligne_i, ligne_j]
+
+                    if mat_num != 0:
+
+                        if mat_num == len(self.fils) + 1:
+                            ligne_p2_x -= self.taille_grid * sens_x
+                            ligne_p2_y -= self.taille_grid * sens_y
+                        else:
+                            self.fil_complet = True
+                        break
+
+                    if ligne_p2_x == x and ligne_p2_y == y:
+                        self.points_apres_pivot.append(QPointF(ligne_p2_x, ligne_p2_y))
+                        break
+
+                    ligne_i, ligne_j = self.agrandir_matrice(ligne_i, ligne_j)
+                    ligne_i += sens_y
+                    ligne_j += sens_x
+                    self.points_apres_pivot.append(QPointF(ligne_p2_x, ligne_p2_y))
+                    ligne_p2_x += self.taille_grid * sens_x
+                    ligne_p2_y += self.taille_grid * sens_y
+
+                ligne.setLine(QLineF(pivot.x(), pivot.y(), ligne_p2_x, ligne_p2_y))
 
     def mat_to_pos(self, i, j):
         x = (j * self.taille_grid) + self.mat_j0
@@ -389,11 +553,15 @@ class GraphicsView(QGraphicsView):
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self.main_window.demarrer_ligne(event.position())
+            self.main_window.clic_gauche_fil(event.position())
+
+        if event.button() == Qt.RightButton:
+            self.main_window.clic_droit_fil()
 
     def mouseMoveEvent(self, event):
         if self.main_window.dessine:
             self.main_window.continuer_dessin(event.position())
+
     """
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton and self.main_window.dessine:
